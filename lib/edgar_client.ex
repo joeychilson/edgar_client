1b9dcb8563aa81d32a1d36d8a666b5137c051ad2
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2023-06-23 17:21:38 -0500

    Even more endpoints

--- lib/edgar_client.ex
+++ lib/edgar_client.ex
@@ -10,16 +10,74 @@ defmodule EDGARClient do
     Supervisor.start_link(children, opts)
   end
 
+  @edgar_archives_url "https://www.sec.gov/Archives/edgar"
+  @edgar_data_url "https://data.sec.gov"
+  @edgar_files_url "https://www.sec.gov/files"
+
+  @doc """
+  Fetches the entity directory
+
+    ## Examples
+
+      iex> {:ok, entity_directory} = EDGARClient.get_entity_directory("320193")
+      iex> entity_directory.directory.name
+      "/Archives/edgar/data/320193"
+  """
+  def get_entity_directory(cik) do
+    cik = String.pad_leading(cik, 10, "0")
+
+    "#{@edgar_archives_url}/data/#{cik}/index.json"
+    |> EDGARClient.get()
+  end
+
+  @doc """
+  Fetches the filing directory
+
+    ## Examples
+
+      iex> {:ok, filing_directory} = EDGARClient.get_filing_directory("320193", "000032019320000010")
+      iex> filing_directory.directory.name
+      "/Archives/edgar/data/320193/000032019320000010"
+  """
+  def get_filing_directory(cik, accession_number) do
+    accession_number = String.replace(accession_number, "-", "")
+
+    "#{@edgar_archives_url}/data/#{cik}/#{accession_number}/index.json"
+    |> EDGARClient.get()
+  end
+
+  @doc """
+  Fetches a list of company tickers
+
+    ## Examples
+
+      iex> {:ok, company_tickers} = EDGARClient.get_company_tickers()
+      iex> Enum.count(company_tickers) > 0
+      true
+  """
+  def get_company_tickers() do
+    resp = EDGARClient.get("#{@edgar_files_url}/company_tickers.json")
+    case resp do
+      {:ok, result} ->
+        {:ok, Map.values(result)}
+      _ ->
+        resp
+    end
+  end
+
   @doc """
   Fetches submissions for a given CIK
 
     ## Examples
 
-      iex> EDGARClient.get_submissions("0000320193")
-      {:ok, submissions_data}
+      iex> {:ok, submissions} = EDGARClient.get_submissions("320193")
+      iex> submissions.cik
+      "320193"
   """
   def get_submissions(cik) do
-    "https://data.sec.gov/submissions/CIK#{cik}.json"
+    cik = String.pad_leading(cik, 10, "0")
+
+    "#{@edgar_data_url}/submissions/CIK#{cik}.json"
     |> EDGARClient.get()
   end
 
@@ -28,11 +86,14 @@ defmodule EDGARClient do
 
     ## Examples
 
-      iex> EDGARClient.get_company_facts("0000320193")
-      {:ok, company_facts_data}
+      iex> {:ok, company_facts} = EDGARClient.get_company_facts("320193")
+      iex> company_facts.cik
+      320193
   """
   def get_company_facts(cik) do
-    "https://data.sec.gov/api/xbrl/companyfacts/CIK#{cik}.json"
+    cik = String.pad_leading(cik, 10, "0")
+
+    "#{@edgar_data_url}/api/xbrl/companyfacts/CIK#{cik}.json"
     |> EDGARClient.get()
   end
 
@@ -41,11 +102,14 @@ defmodule EDGARClient do
 
     ## Examples
 
-      iex> EDGARClient.get_company_concept("0000320193", "us-gaap", "AccountsPayableCurrent")
-      {:ok, company_concept_data}
+      iex> {:ok, company_concept} = EDGARClient.get_company_concept("320193", "us-gaap", "AccountsPayableCurrent")
+      iex> company_concept.cik
+      320193
   """
   def get_company_concept(cik, taxonomy, tag) do
-    "https://data.sec.gov/api/xbrl/companyconcept/CIK#{cik}/#{taxonomy}/#{tag}.json"
+    cik = String.pad_leading(cik, 10, "0")
+
+    "#{@edgar_data_url}/api/xbrl/companyconcept/CIK#{cik}/#{taxonomy}/#{tag}.json"
     |> EDGARClient.get()
   end
 
@@ -54,11 +118,12 @@ defmodule EDGARClient do
 
     ## Examples
 
-      iex> EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1I")
-      {:ok, frames_data}
+      iex> {:ok, frames} = EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1I")
+      iex> frames.tag
+      "AccountsPayableCurrent"
   """
   def get_frames(taxonomy, tag, unit, period) do
-    "https://data.sec.gov/api/xbrl/frames/#{taxonomy}/#{tag}/#{unit}/#{period}.json"
+    "#{@edgar_data_url}/api/xbrl/frames/#{taxonomy}/#{tag}/#{unit}/#{period}.json"
     |> EDGARClient.get()
   end
 
@@ -69,19 +134,27 @@ defmodule EDGARClient do
 
   ## Examples
 
-    iex> EDGARClient.get("https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json")
-    {:ok, company_facts_data}
+    iex> {:ok, company_facts} = EDGARClient.get("https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json")
+    iex> company_facts.cik
+    320193
   """
   def get(url) do
     SimpleRateLimiter.wait_and_proceed(fn ->
-      resp = HTTPoison.get(url, [{"User-Agent", "example <example@example.com>"}])
+      resp =
+        HTTPoison.get(url, [{"User-Agent", "example <example@example.com>"}],
+          follow_redirect: true
+        )
+
       case resp do
         {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
           {:ok, Jason.decode!(body, keys: :atoms)}
+
         {:ok, %HTTPoison.Response{status_code: 404}} ->
           {:error, :not_found}
+
         {:ok, %HTTPoison.Response{status_code: code}} ->
           {:error, {:unexpected_status_code, code}}
+
         {:error, %HTTPoison.Error{reason: reason}} ->
           {:error, reason}
       end
--- test/edgar_client_test.exs
+++ test/edgar_client_test.exs
@@ -2,43 +2,71 @@ defmodule EDGARClientTest do
   use ExUnit.Case
   doctest EDGARClient
 
+  test "entity directory found" do
+    entity_directory = EDGARClient.get_entity_directory("320193")
+    assert {:ok, entity_directory} = entity_directory
+    assert entity_directory.directory.name == "/Archives/edgar/data/320193"
+  end
+
+  test "entity directory not found" do
+    entity_directory = EDGARClient.get_entity_directory("1")
+    assert {:error, :not_found} = entity_directory
+  end
+
+  test "filing directory found" do
+    filing_directory = EDGARClient.get_filing_directory("320193", "000032019320000010")
+    assert {:ok, filing_directory} = filing_directory
+    assert filing_directory.directory.name == "/Archives/edgar/data/320193/000032019320000010"
+  end
+
+  test "filing directory not found" do
+    filing_directory = EDGARClient.get_filing_directory("1", "1")
+    assert {:error, :not_found} = filing_directory
+  end
+
+  test "company tickers found" do
+    company_tickers = EDGARClient.get_company_tickers()
+    assert {:ok, company_tickers} = company_tickers
+    assert Enum.count(company_tickers) > 0
+  end
+
   test "submissions found" do
-    submissions = EDGARClient.get_submissions("0000320193")
-    assert {:ok, submissions_data} = submissions
-    assert submissions_data.cik == "320193"
+    submissions = EDGARClient.get_submissions("320193")
+    assert {:ok, submissions} = submissions
+    assert submissions.cik == "320193"
   end
 
   test "submissions not found" do
-    submissions = EDGARClient.get_submissions("0000320")
+    submissions = EDGARClient.get_submissions("1")
     assert {:error, :not_found} = submissions
   end
 
   test "company facts found" do
-    company_facts = EDGARClient.get_company_facts("0000320193")
-    assert {:ok, company_facts_data} = company_facts
-    assert company_facts_data.cik == 320193
+    company_facts = EDGARClient.get_company_facts("320193")
+    assert {:ok, company_facts} = company_facts
+    assert company_facts.cik == 320193
   end
 
   test "company facts not found" do
-    company_facts = EDGARClient.get_company_facts("0000320")
+    company_facts = EDGARClient.get_company_facts("1")
     assert {:error, :not_found} = company_facts
   end
 
   test "company concept found" do
-    company_concept = EDGARClient.get_company_concept("0000320193", "us-gaap", "AccountsPayableCurrent")
-    assert {:ok, company_concept_data} = company_concept
-    assert company_concept_data.cik == 320193
+    company_concept = EDGARClient.get_company_concept("320193", "us-gaap", "AccountsPayableCurrent")
+    assert {:ok, company_concept} = company_concept
+    assert company_concept.cik == 320193
   end
 
   test "company concept not found" do
-    company_concept = EDGARClient.get_company_concept("0000320", "us-gaap", "AccountsPayableCurrent")
+    company_concept = EDGARClient.get_company_concept("1", "us-gaap", "AccountsPayableCurrent")
     assert {:error, :not_found} = company_concept
   end
 
   test "frames found" do
     frames = EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1I")
-    assert {:ok, frames_data} = frames
-    assert frames_data.tag == "AccountsPayableCurrent"
+    assert {:ok, frames} = frames
+    assert frames.tag == "AccountsPayableCurrent"
   end
 
   test "frames not found" do
