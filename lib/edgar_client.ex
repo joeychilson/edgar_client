8baf3648591b88b4ee71a5b21beb0290d7959d36
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2023-06-23 15:08:30 -0500

    More endpoints and better tests

--- lib/edgar_client.ex
+++ lib/edgar_client.ex
@@ -12,6 +12,11 @@ defmodule EDGARClient do
 
   @doc """
   Fetches submissions for a given CIK
+
+    ## Examples
+
+      iex> EDGARClient.get_submissions("0000320193")
+      {:ok, submissions_data}
   """
   def get_submissions(cik) do
     "https://data.sec.gov/submissions/CIK#{cik}.json"
@@ -19,7 +24,53 @@ defmodule EDGARClient do
   end
 
   @doc """
+  Fetches company facts for a given CIK
+
+    ## Examples
+
+      iex> EDGARClient.get_company_facts("0000320193")
+      {:ok, company_facts_data}
+  """
+  def get_company_facts(cik) do
+    "https://data.sec.gov/api/xbrl/companyfacts/CIK#{cik}.json"
+    |> EDGARClient.get()
+  end
+
+  @doc """
+  Fetches company concepts for a given CIK and concept (taxonomy, tag)
+
+    ## Examples
+
+      iex> EDGARClient.get_company_concept("0000320193", "us-gaap", "AccountsPayableCurrent")
+      {:ok, company_concept_data}
+  """
+  def get_company_concept(cik, taxonomy, tag) do
+    "https://data.sec.gov/api/xbrl/companyconcept/CIK#{cik}/#{taxonomy}/#{tag}.json"
+    |> EDGARClient.get()
+  end
+
+  @doc """
+  Fetches frames for a given taxonomy, concept, unit, and period
+
+    ## Examples
+
+      iex> EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1I")
+      {:ok, frames_data}
+  """
+  def get_frames(taxonomy, tag, unit, period) do
+    "https://data.sec.gov/api/xbrl/frames/#{taxonomy}/#{tag}/#{unit}/#{period}.json"
+    |> EDGARClient.get()
+  end
+
+  @doc """
   Makes a GET request to the specified URL, returns parsed JSON body if successful
+  The request is rate limited to 10 requests per second to avoid hitting the SEC rate limit
+  The User-Agent header is set to abide by the Internet Security Policy.
+
+  ## Examples
+
+    iex> EDGARClient.get("https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json")
+    {:ok, company_facts_data}
   """
   def get(url) do
     SimpleRateLimiter.wait_and_proceed(fn ->
--- test/edgar_client_test.exs
+++ test/edgar_client_test.exs
@@ -2,13 +2,47 @@ defmodule EDGARClientTest do
   use ExUnit.Case
   doctest EDGARClient
 
-  test "test submissions found" do
+  test "submissions found" do
     submissions = EDGARClient.get_submissions("0000320193")
-    assert {:ok, _} == submissions
+    assert {:ok, submissions_data} = submissions
+    assert submissions_data.cik == "320193"
   end
 
-  test "test submissions not found" do
+  test "submissions not found" do
     submissions = EDGARClient.get_submissions("0000320")
-    assert {:error, :not_found} == submissions
+    assert {:error, :not_found} = submissions
+  end
+
+  test "company facts found" do
+    company_facts = EDGARClient.get_company_facts("0000320193")
+    assert {:ok, company_facts_data} = company_facts
+    assert company_facts_data.cik == 320193
+  end
+
+  test "company facts not found" do
+    company_facts = EDGARClient.get_company_facts("0000320")
+    assert {:error, :not_found} = company_facts
+  end
+
+  test "company concept found" do
+    company_concept = EDGARClient.get_company_concept("0000320193", "us-gaap", "AccountsPayableCurrent")
+    assert {:ok, company_concept_data} = company_concept
+    assert company_concept_data.cik == 320193
+  end
+
+  test "company concept not found" do
+    company_concept = EDGARClient.get_company_concept("0000320", "us-gaap", "AccountsPayableCurrent")
+    assert {:error, :not_found} = company_concept
+  end
+
+  test "frames found" do
+    frames = EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1I")
+    assert {:ok, frames_data} = frames
+    assert frames_data.tag == "AccountsPayableCurrent"
+  end
+
+  test "frames not found" do
+    frames = EDGARClient.get_frames("us-gaap", "AccountsPayableCurrent", "USD", "CY2019Q1")
+    assert {:error, :not_found} = frames
   end
 end
