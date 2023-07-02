d420d98d8f85b91158b54ec88dfe453547468a43
Joey Chilson <joeychilson@outlook.com>
2023-07-02 15:50:36 -0500

    v0.2.1

--- lib/edgar/native.ex
+++ lib/edgar/native.ex
@@ -10,18 +10,8 @@ defmodule EDGAR.Native do
     crate: "edgar",
     version: version,
     base_url: "#{github_url}/releases/download/v#{version}",
-    targets: ~w(
-      aarch64-apple-darwin
-      aarch64-unknown-linux-gnu
-      aarch64-unknown-linux-musl
-      riscv64gc-unknown-linux-gnu
-      x86_64-apple-darwin
-      x86_64-pc-windows-msvc
-      x86_64-pc-windows-gnu
-      x86_64-unknown-linux-gnu
-      x86_64-unknown-linux-musl
-      x86_64-unknown-freebsd
-    ),
+    targets:
+      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
     mode: mode,
     force_build: System.get_env("EDGAR_CLIENT_BUILD") in ["1", "true"]
 
